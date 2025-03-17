8d456bfe5fef02bc024c48a3b2c291b5c40f53bb
Bitpoke-YT <mrmwgames@gmail.com>
2025-03-17 14:23:33 +1100

    Added Database Setuo

--- Database.py
+++ Database.py
@@ -1,5 +1,53 @@
 import sqlite3
+import os
 
 class EncounterDatabase():
-    server = sqlite3.connect('Encounter.db')
-    
+
+    server = "Temp Server"
+
+    def SetupTables(self):
+        if self.server == "Temp Server":
+            raise "Server Not Setup yet"
+            return
+        cursor = self.server.cursor()
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Encounters (
+            EncounterID INTEGER,
+            Name TEXT,
+            CR INTEGER
+        )
+        ''')
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Enemys (
+            EnemyID INTEGER,
+            Name TEXT,
+            Initiative INTEGER,
+            CR INTEGER
+        )
+        ''')
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS EncounterEnemys (
+            EncounterID INTEGER,
+            EnemyID INTEGER,
+            PRIMARY KEY (EncounterID, EnemyID),
+            FOREIGN KEY (EncounterID) REFERENCES Encounters(EncounterID)
+            FOREIGN KEY (EnemyID) REFERENCES Enemys(EnemyID)
+        )
+        ''')
+
+
+    def __init__(self):
+        path = os.path.expanduser('~/Documents/Dnd_Encounter')
+        if not os.path.exists(path):
+            os.makedirs(path)
+        try:
+            self.server = sqlite3.connect(f"{path}/Encounter.db")
+            self.SetupTables()
+        except Exception as e:
+            print(f"Count Not Connect to server This is the Issue {e}")
+
+
+
+
+test = EncounterDatabase()

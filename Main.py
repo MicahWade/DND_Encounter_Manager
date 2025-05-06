4a731fcff7191d00fb95e56760f37d94b676d189
s0172606 <0172606@schoolsnet.act.edu.au>
2025-05-06 15:12:39 +1000

    Setup Dice Roll popup

--- Main.py
+++ Main.py
@@ -134,7 +134,6 @@ def settings():
 
 @app.route('/favicon.ico')
 def favicon():
-    print("Test")
     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
